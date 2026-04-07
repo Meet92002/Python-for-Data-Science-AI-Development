39414245d6dde9bc3bb80600b7411553be2cb3ec
Meet92002 <meet92002@gmail.com>
2026-04-07 10:41:47 +0530

    First commit

---
+++ First_program.py
@@ -0,0 +1,37 @@
+# File of Contents
+
+# 'Hello' to the world in Python
+# What version of Python are we using?
+# Writing comments in Python
+# Errors in Python
+# Does Python know about your error before it runs your code?
+# Exercise: Your First Program
+
+# 1. Say 'Hello' to the world in Python
+print('Hello, Python!')
+
+# 2. What version of Python are we using?
+
+import sys
+print(sys.version)
+
+# 3. Practice on writing comments
+
+print('Hello, Python!') # This line prints a string
+# print("Hi")
+
+
+# 4. Errors in Python
+
+print('Hello, Python!')
+frint("Hello, Python!")
+ # Try to see built-in error message
+print("Hello, Python!)
+
+# 5. Python know about your error before it runs your code
+
+# Print string and error to see the running order
+
+print("This will be printed")
+frint("This will cause an error")
+print("This will NOT be printed")
