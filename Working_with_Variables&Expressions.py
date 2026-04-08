97de4c3a9f506cc9a221dbb1f79c73fbe468f417
Meet Patel <meet92002@gmail.com>
2026-04-09 00:09:48 +0530

    Working_with_Variables&Expressions

---
+++ Working_with_Variables&Expressions.py
@@ -0,0 +1,67 @@
+# Expressions and Variables
+# Expressions
+# Exercise: Expressions
+# Variables
+# Exercise: Expression and Variables in Python
+
+# Addition operation expression
+
+43 + 60 + 16 + 41 # out put - 160
+
+# Subtraction operation expression
+
+50 - 60 # output - -10
+
+# Multiplication operation expression
+
+5 * 5 # output - 25
+
+# Division operation expression
+
+25 / 5 #output - 5.0
+
+# Integer division operation expression
+
+25 // 5 #output - 5
+
+# Write your code below. Don't forget to press Shift+Enter to execute the cell
+160//60 #output - 2
+
+# Mathematical expression
+
+(30 + 2) * 60 #output - 1920
+
+# Store value into variable
+
+x = 43 + 60 + 16 + 41 #output - 160
+
+# Print out the value in variable
+x = 43 + 60 + 16 + 41 #output - 160
+x
+
+# Use another variable to store the result of the operation between variable and value
+
+y = x / 60  # output - 2.6666666666666665
+y
+
+# Overwrite variable with new value
+x = 43 + 60 + 16 + 41  #output - 160
+x = x / 60
+x
+
+# Name the variables meaningfully
+
+total_min = 43 + 42 + 57 # Total length of albums in minutes
+total_min   #output - 142
+
+# Name the variables meaningfully
+total_min = 43 + 42 + 57
+total_hours = total_min / 60 # Total length of albums in hours 
+total_hours    #output - 2.3666666666666667
+
+# ComplicateD expression
+
+total_hours = (43 + 42 + 57) / 60  # Total hours in a single expression
+total_hours    #output - 2.3666666666666667
+
+
