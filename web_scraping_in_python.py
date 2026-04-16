092ea36b381bcaa65223275021f8defa2ec6acd5
Meet Patel <meet92002@gmail.com>
2026-04-16 10:09:01 +0530

    web scraping tables using pandas

--- web_scraping_in_python.py
+++ web_scraping_in_python.py
@@ -20,7 +20,7 @@ soup = BeautifulSoup(page.content, "html.parser")
 # Scrapy is an open-source and collaborative web crawling framework for Python. 
 # It is used to extract the data from the website
 
-import scrapy
+import scrapy # type: ignore
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = ['http://quotes.toscrape.com/tag/humor/',]
@@ -31,6 +31,23 @@ class QuotesSpider(scrapy.Spider):
 # Selenium:- 
 # Selenium is a tool used for controlling web browsers through programs and automating browser tasks.
 
-from selenium import webdriver
+from selenium import webdriver # type: ignore
 driver = webdriver.Firefox()
 driver.get("http://www.example.com")
+
+# Web Scraping Tables using Pandas
+
+# The Pandas library in Python contains a function read_html() 
+# that can be used to extract tabular information from any web page.
+
+import pandas as pd
+URL = 'https://en.wikipedia.org/wiki/List_of_largest_banks'
+tables = pd.read_html(URL)
+df = tables[0]
+print(df)   
+
+
+URL = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)'
+tables = pd.read_html(URL)
+df = tables[2] # the required table will have index 2
+print(df)
